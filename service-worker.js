4f81252d239bebb95bb69fb0c09f12615ea9ec74
Edison Peguero <98351919+Pegueroedison@users.noreply.github.com>
2026-05-28 00:50:28 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const WT_CACHE = "wt-guide-rd-v4039-profile-scroll-name-limit";
+const WT_CACHE = "wt-guide-rd-v4040-profile-scroll-name-limit";
 self.addEventListener("install", event => { self.skipWaiting(); });
 self.addEventListener("activate", event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== WT_CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
